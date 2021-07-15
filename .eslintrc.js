9d6e34ac10485966ddcf4a894aa84239e20456d3
getlarge <ed@getlarge.eu>
2021-07-15 13:30:41 +0200

    chore: update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
+    'prettier',
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
 };
